35d2313164c80007a1981567110423e0ea9b5084
Bruno Duarte <brunoalvimduarte@gmail.com>
2024-12-12 20:55:57 -0300

    feat: adding the first part of the login page

---
+++ frontend/src/components/Login/index.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import Button from "../design-system/Button";
+
+const Login = () => {
+  return (
+    <main className="w-screen h-screen flex flex-col items-center justify-center relative overflow-hidden bg-loginBG">
+     <div>
+        <h1>Sobre o Best Chat</h1>
+        <p>Best Chat é um projeto criado por Bruno Alvim Duarte. Ele foi criado com a intenção de expor meus conhecimentos em NextJS e NestJS. Lorem ipsum
+            
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dapibus luctus nisl a mattis. Cras feugiat varius lectus nec lacinia. Ut sed elit suscipit, tristique libero et, mollis magna. Pellentesque pharetra elit velit, dapibus dapibus sem viverra viverra. Ut condimentum at arcu ac accumsan. Phasellus a scelerisque est. Sed sit amet turpis leo.
+
+        Aenean dapibus leo felis. Aenean nec convallis sapien. Donec elit urna, viverra ut hendrerit a, tempor quis risus. Aenean ligula est, pharetra vitae consectetur ut, lobortis at turpis. Ut ac quam nulla. Mauris bibendum ex massa, ac dignissim turpis mattis et. Duis a elementum lacus.
+
+        Vestibulum quis maximus mauris, id malesuada tortor. Donec aliquet risus a arcu tristique ornare. Donec mollis pretium massa non tincidunt. Nullam ac ex nisi. Maecenas viverra elit a consectetur accumsan. Donec vel cursus lectus, vitae faucibus ipsum. Integer sed lacinia tellus. Mauris ac sapien lorem.
+        </p>
+     </div>
+     <div>
+        <Button variant="secondary">Testandoooo</Button>
+     </div>
+    </main>
+  );
+};
+
+export default Login;
